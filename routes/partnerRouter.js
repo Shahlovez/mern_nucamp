f4f296aaa7e052ae030c3d6751ab53012c525083
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-10 23:04:19 -0400

    commented out comments

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -136,85 +136,85 @@ partnerRouter.route("/:partnerId/comments")
       .catch(err => next(err));
   });
 
-partnerRouter.route("/:partnerId/comments/:commentId")
-  .get((req, res, next) => {
-    Partner.findById(req.params.partnerId)
-      .then(partner => {
-        if (partner && partner.comments.id(req.params.commentId)) {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(partner.comments.id(req.params.commentId));
-        } else if (!partner) {
-          err = new Error(`partner ${req.params.partnerId} not found`);
-          err.status = 404;
-          return next(err);
-        } else {
-          const err = new Error(`Comment ${req.params.commentId} not found`);
-          err.status = 404;
-          return next(err);
-        }
-      })
-      .catch((err) => next(err));
-  })
-  .post((req, res) => {
-    res.statusCode = 403;
-    res.end(
-      `POST operation not supported on /partners/${req.params.partnerId}/comments/${req.params.commentId}`
-    );
-  })
-  .put((req, res, next) => {
-    Partner.findById(req.params.partnerId)
-      .then((partner) => {
-        if (partner && partner.comments.id(req.params.commentId)) {
-          if (req.body.rating) {
-            partner.comments.id(req.params.commentId).rating = req.body.rating;
-          }
-          if (req.body.text) {
-            partner.comments.id(req.params.commentId).text = req.body.text;
-          }
-          partner
-            .save()
-            .then((partner) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(partner);
-            })
-            .catch((err) => next(err));
-        } else if (!partner) {
-          err = new Error(`partner ${req.params.partnerId} not found`);
-          err.status = 404;
-          return next(err);
-        } else {
-          const err = new Error(`Comment ${req.params.commentId} not found`);
-          err.status = 404;
-          return next(err);
-        }
-      })
-      .catch((err) => next(err));
-  })
-  .delete((req, res, next) => {
-    Partner.findById(req.params.partnerId)
-      .then((partner) => {
-        if (partner && partner.comments.id(req.params.commentId)) {
-          partner.comments.id(req.params.commentId).remove();
-          partner
-            .save()
-            .then((partner) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(partner);
-            })
-            .catch((err) => next(err));
-        } else if (!partner) {
-          err = new Error(`partner ${req.params.partnerId} not found`);
-          err.status = 404;
-          return next(err);
-        } else {
-          const err = new Error(`Comment ${req.params.commentId} not found`);
-          err.status = 404;
-          return next(err);
-        }
-      })
-      .catch(err => next(err));
-  });
+// partnerRouter.route("/:partnerId/comments/:commentId")
+//   .get((req, res, next) => {
+//     Partner.findById(req.params.partnerId)
+//       .then(partner => {
+//         if (partner && partner.comments.id(req.params.commentId)) {
+//           res.statusCode = 200;
+//           res.setHeader("Content-Type", "application/json");
+//           res.json(partner.comments.id(req.params.commentId));
+//         } else if (!partner) {
+//           err = new Error(`partner ${req.params.partnerId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         } else {
+//           const err = new Error(`Comment ${req.params.commentId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         }
+//       })
+//       .catch((err) => next(err));
+//   })
+//   .post((req, res) => {
+//     res.statusCode = 403;
+//     res.end(
+//       `POST operation not supported on /partners/${req.params.partnerId}/comments/${req.params.commentId}`
+//     );
+//   })
+//   .put((req, res, next) => {
+//     Partner.findById(req.params.partnerId)
+//       .then((partner) => {
+//         if (partner && partner.comments.id(req.params.commentId)) {
+//           if (req.body.rating) {
+//             partner.comments.id(req.params.commentId).rating = req.body.rating;
+//           }
+//           if (req.body.text) {
+//             partner.comments.id(req.params.commentId).text = req.body.text;
+//           }
+//           partner
+//             .save()
+//             .then((partner) => {
+//               res.statusCode = 200;
+//               res.setHeader("Content-Type", "application/json");
+//               res.json(partner);
+//             })
+//             .catch((err) => next(err));
+//         } else if (!partner) {
+//           err = new Error(`partner ${req.params.partnerId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         } else {
+//           const err = new Error(`Comment ${req.params.commentId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         }
+//       })
+//       .catch((err) => next(err));
+//   })
+//   .delete((req, res, next) => {
+//     Partner.findById(req.params.partnerId)
+//       .then((partner) => {
+//         if (partner && partner.comments.id(req.params.commentId)) {
+//           partner.comments.id(req.params.commentId).remove();
+//           partner
+//             .save()
+//             .then((partner) => {
+//               res.statusCode = 200;
+//               res.setHeader("Content-Type", "application/json");
+//               res.json(partner);
+//             })
+//             .catch((err) => next(err));
+//         } else if (!partner) {
+//           err = new Error(`partner ${req.params.partnerId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         } else {
+//           const err = new Error(`Comment ${req.params.commentId} not found`);
+//           err.status = 404;
+//           return next(err);
+//         }
+//       })
+//       .catch(err => next(err));
+//   });
 module.exports = partnerRouter;
