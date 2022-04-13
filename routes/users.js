3ae469de43c5644bb890189302b811aee7e2b1ed
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-13 11:40:16 -0400

    Updated the user router

--- routes/users.js
+++ routes/users.js
@@ -9,25 +9,39 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-    router.post('/signup', (req, res) => {
-        User.register(
-            new User({username: req.body.username}),
-            req.body.password,
-            err => {
-                if (err) {
-                    res.statusCode = 500;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json({err: err});
-                } else {
+router.post('/signup', (req, res) => {
+    User.register(
+        new User({username: req.body.username}),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({err: err});
+            } else {
+                if (req.body.firstname) {
+                    user.firstname = req.body.firstname;
+                }
+                if (req.body.lastname) {
+                    user.lastname = req.body.lastname;
+                }
+                user.save(err => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({err: err});
+                        return;
+                    }
                     passport.authenticate('local')(req, res, () => {
                         res.statusCode = 200;
                         res.setHeader('Content-Type', 'application/json');
                         res.json({success: true, status: 'Registration Successful!'});
                     });
-                }
+                });
             }
-        );
-    });
+        }
+    );
+});
 
     router.post('/login', passport.authenticate('local'), (req, res) => {
         const token = authenticate.getToken({_id: req.user._id});
