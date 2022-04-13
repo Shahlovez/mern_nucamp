8bf0399b8c022a546056941ba9334fc77515ba49
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-13 05:44:58 -0400

    Added Token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const passport = require('passport');
+const authenticate = require('../authenticate');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -29,9 +30,10 @@ router.get('/', function(req, res, next) {
     });
 
     router.post('/login', passport.authenticate('local'), (req, res) => {
+        const token = authenticate.getToken({_id: req.user._id});
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'You are successfully logged in!'});
+        res.json({success: true, token: token, status: 'You are successfully logged in!'});
     });
 
     router.get('/logout', (req, res, next) => {
