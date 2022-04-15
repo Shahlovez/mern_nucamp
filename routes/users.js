a8c1be5016fce8f57f062d7397bc07ca0fcd7981
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-15 10:56:29 -0400

    Admin access to users documents

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,15 @@ const authenticate = require('../authenticate');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    User.find()
+    .then(users => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    })
+    .catch(err => next(err));
+});
 router.post('/signup', (req, res) => {
     User.register(
         new User({username: req.body.username}),
