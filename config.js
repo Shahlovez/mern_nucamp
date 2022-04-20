ee017dfa70307ef838e8de18e3f7d3c3cc81187e
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-19 20:40:46 -0400

    Deleted config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '297581065732475',
-        clientSecret: '2abf92933708dd57fac7dd60ccdef7ed'
-    }
-}
