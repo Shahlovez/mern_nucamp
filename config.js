d1cb49fac80df01af571e6a7a8905f56ec15cf2f
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-19 20:27:39 -0400

    Added App ID and App Secret

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '297581065732475',
+        clientSecret: '2abf92933708dd57fac7dd60ccdef7ed'
+    }
 }
