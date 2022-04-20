9e3a733a129d3df011c4961d162074dda5a725a5
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-19 22:14:09 -0400

    deleted

--- models/user.js
+++ models/user.js
@@ -14,10 +14,9 @@ const userSchema = new Schema({
     admin: {
         type: Boolean,
         default: false
-    }
+    }, 
+    facebookId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
-
-
 module.exports = mongoose.model('User', userSchema);
