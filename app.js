cd150d0539c1c685b2239b403c1d60f6e2b36b00
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-04-23 12:30:09 -0400

    Integrated favoriteRouter into the express app

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
 
 const connect = mongoose.connect(url, {
-
   useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+  useFindAndModify: false,
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 });
 
 connect.then(() => console.log('Connected correctly to server'), 
@@ -59,6 +59,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
